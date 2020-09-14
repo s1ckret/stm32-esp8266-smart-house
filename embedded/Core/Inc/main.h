fb576465a52953516a81cff5f18e20d1a3e3f86c
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-14 08:34:33 +0300

    build(stm32): add button pinout
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- embedded/Core/Inc/main.h
+++ embedded/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Button_Pin GPIO_PIN_11
+#define Button_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
