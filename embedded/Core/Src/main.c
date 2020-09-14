1eed9f21907316ef7361272488737a34f523aea5
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-14 08:34:33 +0300

    build(stm32): add button pinout
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- embedded/Core/Src/main.c
+++ embedded/Core/Src/main.c
@@ -216,11 +216,18 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin : Button_Pin */
+  GPIO_InitStruct.Pin = Button_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(Button_GPIO_Port, &GPIO_InitStruct);
 
 }
 
