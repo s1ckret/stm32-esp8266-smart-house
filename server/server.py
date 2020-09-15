2ace7ea1d5b7780b91d6060c2e3964f29c876b85
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-15 22:23:17 +0300

    feat(server): form http response
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,10 +1,11 @@
 import socket
 import threading
+import os
 
 # Check your ip with "ipconfig" cmd
 host='192.168.0.107'
 port=30000
-
+fileRoot = "../webpage/"
 # @status - int
 def response_header(status):
     return "HTTP/1.1 {} OK\n".format(status)
@@ -18,22 +19,45 @@ def response_content_length(len):
     return "Content-Length: {}\n\n".format(len)
 
 def handle_get_request(args):
-    pass
+    file_name = args[1]
+    # TODO: Add file type parsing
+    # Example: if .html then content_type = text/html
+    # Example: is .css then content_type = text/css
+    if (file_name == "/"): 
+        file_name = "index.html"
+
+    response = ""
+    if (os.path.isfile(fileRoot + file_name)):
+        file = open(fileRoot + file_name)
+        file_str = file.read()
+        file_len = len(file_str)
+
+        response += response_header(200)
+        response += response_content_type("text/html")
+        response += response_content_length(file_len)
+        response += file_str
+    else:
+        response += "ERROR: There is no file {}".format(fileRoot + file_name)
+    return response
 
 def handle_http_request(request, args):
+    response = ""
     if (request == "GET"):
-        handle_get_request(args)
+        response = handle_get_request(args)
+    return response
 
 def handle_client(conn, address):
     print(f"Connection from {address} has been established.")
 
     while True:
         msg = conn.recv(2048).decode("utf-8")
-        print(f"Message received:\n==========\n{msg}\n==========\n")
+        print(f"Request\n{msg}\n")
         args = msg.split(" ", 2)
         # TODO: pass better args
-        handle_http_request(args[0], args)
-        conn.send("Msg received".encode("utf-8"))
+        response = handle_http_request(args[0], args)
+        print(f"Response\n{response}\n")
+        conn.send(response.encode("utf-8"))
+        break
     conn.close()
     print(f"Connection from {address} has been closed.")
 
