2c4958d550bcdc219015728eeccbf0fd79be1528
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-19 20:00:04 +0300

    feat(server): add parse_file_extension function to the http
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- server/http.py
+++ server/http.py
@@ -13,13 +13,18 @@ def response_content_type(type):
 def response_content_length(len):
     return "Content-Length: {}\n\n".format(len)
 
+def parse_file_extension(file_name):
+    return "text/html"
+
 def handle_get_request(file_name):
     # TODO: Add file type parsing
     # Example: if .html then content_type = text/html
     # Example: is .css then content_type = text/css
     if (file_name == "/"): 
         file_name = "index.html"
-
+    
+    content_type = parse_file_extension(file_name)
+    
     response = ""
     try:
         with open(fileRoot + file_name, 'r') as file:
@@ -27,7 +32,7 @@ def handle_get_request(file_name):
             file_len = len(file_str)
 
             response += response_header(200)
-            response += response_content_type("text/html")
+            response += response_content_type(content_type)
             response += response_content_length(file_len)
             response += file_str
             return response
