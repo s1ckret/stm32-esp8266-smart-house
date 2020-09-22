ffa6b81972eccc5d94faa2f5e65fece800b08af7
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-22 23:39:58 +0300

    feat(sttp): add file writing
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- server/sttp.py
+++ server/sttp.py
@@ -11,7 +11,16 @@ def handle_R_frame(msg):
     sensor_id = msg[1]
     payload_size = msg[2]
     payload = msg[3:]
-    # TODO: Write into the file
+    data = int.from_bytes(payload, byteorder="big")
+    print(f"Got frame: sensor ID: {sensor_id}, payload size: {payload_size}, payload: {payload}\nConverted data: {data} ")
+    file_name = dict_sensor_id_to_file[sensor_id]
+    try:
+        with open(constants.fileRoot + file_name, 'wb') as file:
+            # TODO: add support for different payload size
+            file.write(str(data).encode(constants.encoding))
+    except OSError as e:
+        print("ERROR #{} while writing into {}".format(e.errno, constants.fileRoot + file_name))
+        return b"ERROR"
     return b"OK"
 
 def handle_sttp_msg(msg):
