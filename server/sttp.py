e5c37145325c7c5d9db9038c212c6c86e15f94f1
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-09-22 22:16:40 +0300

    feat(sttp): add handle for R frame
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- server/sttp.py
+++ server/sttp.py
@@ -1,7 +1,11 @@
 # S1ckret Team Transfer Protocol
 
 def handle_R_frame(msg):
-    pass
+    sensor_id = msg[1]
+    payload_size = msg[2]
+    payload = msg[3:]
+    # TODO: Write into the file
+    return b"OK"
 
 def handle_sttp_msg(msg):
     response = b""
