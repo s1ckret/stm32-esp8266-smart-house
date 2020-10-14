9e70034a40a50d14ca2bebde0bfab15bed4e75bd
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-10-14 17:25:04 +0300

    feat(stm32): add led driver
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ embedded/Core/Inc/drv/drv_led.h
@@ -0,0 +1,21 @@
+/*
+ * drv_led.h
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: S1ckret
+ */
+
+#ifndef INC_DRV_DRV_LED_H_
+#define INC_DRV_DRV_LED_H_
+
+enum drv_led_name {
+  DRV_LED_HEARTBEAT = 0,
+  DRV_LED_LIGHT,
+  DRV_LED_COUNT
+};
+
+void drv_led_on(enum drv_led_name me);
+
+void drv_led_off(enum drv_led_name me);
+
+#endif /* INC_DRV_DRV_LED_H_ */
