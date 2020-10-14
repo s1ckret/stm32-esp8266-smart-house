3a38596bf7ab292f0a90c7ee2f828b44d5abedc2
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-10-14 19:54:28 +0300

    build(button): change interrupt trigger to falling edge
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- embedded/Core/Src/main.c
+++ embedded/Core/Src/main.c
@@ -251,7 +251,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : Button_Pin */
   GPIO_InitStruct.Pin = Button_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(Button_GPIO_Port, &GPIO_InitStruct);
 
